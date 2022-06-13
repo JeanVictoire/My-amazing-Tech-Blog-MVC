da48fd120ac03d70896ca0e5c20f33f44e6b9ad6
JeanVictoire <jeanthecoders@gmail.com>
2022-06-13 12:05:54 -0400

    adding some stuff

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
 
 // Configure and link a session object with the sequelize store
 // const sess = {
@@ -28,8 +29,8 @@ const PORT = process.env.PORT || 3001;
 // Add express-session and store as Express.js middleware
 // app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
