ae7cded497be9861b19c8314df34e11f52a0a372
JeanVictoire <jeanthecoders@gmail.com>
2022-06-13 11:37:40 -0400

    Back-End Setup

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS jv_blog_db;
+CREATE DATABASE jv_blog_db;
